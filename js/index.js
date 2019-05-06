743148ea3da2ff1d198e4e35cb4295157ef62b98
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-05-06 19:19:04 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,6 +1,11 @@
 const rootURL = 'https://api.github.com';
 
 function getRepositories() {
+ let username = document.getElementById('username').value;
+ const req = new XMLHttpRequest();
+ req.addEventListener('load', displayRepositories);
+ req.open('GET', 'https://api.github.com/users/' + username + '/repos');
+ req.send();
 }
 
 function displayRepositories() {
