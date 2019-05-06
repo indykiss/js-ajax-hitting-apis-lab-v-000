ac3475edf3c0c1d164ecea020f1ab73a1462c458
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-05-06 19:17:30 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -1,1 +1,19 @@
-// your code here
+const rootURL = 'https://api.github.com';
+
+function getRepositories() {
+}
+
+function displayRepositories() {
+}
+
+function getCommits(el) {
+}
+
+function displayCommits() {
+}
+
+function getBranches(el) {
+}
+
+function displayBranches() {
+}
