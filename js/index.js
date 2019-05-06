58d52763d3d430bf93d22fb1f73533f84c94159a
Indira Kissoondyal <indirak@alumni.princeton.edu>
2019-05-06 19:20:01 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -61,4 +61,16 @@ function getBranches(el) {
 }
 
 function displayBranches() {
+  const branches = JSON.parse(this.responseText);
+ console.log(branches);
+ const branchesList = `<ul>${branches
+   .map(
+     branch =>
+       '<li><strong>' +
+       branch.name +
+       '</strong>' +
+       '</li>'
+   )
+   .join('')}</ul>`;
+ document.getElementById('details').innerHTML = branchesList;
 }
